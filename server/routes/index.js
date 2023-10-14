e71e3c730bc173b24abb19ba29173e16ae31393a
Chamod Dilpa Gunarathna <dilrangachanuka843@gmail.com>
2023-10-14 13:12:15 +0530

    donation

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import user from "./user.js";
+import donation from "./donation.js";
 
 
 const router = express.Router();
 
 router.use("/user", user);
+router.use("/donation", user);
 
 
 export default router;
